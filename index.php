9c246b58e4c2a2045c2eea9ae70f6300ded35cdd
Trần Quốc Nam <quocnam901@gmail.com>
2017-06-03 21:15:49 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,25 +1,69 @@
-<?php
-	session_start();
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Trang Chủ</title>
+
+<title>Quản lý Cửa Hàng Tạp Hóa</title>
+
+<link type="text/css" rel="stylesheet" href="style/css.css" />
 </head>
-<body bgcolor="#FFFF33">
-<table width="980" height="600" border="0" align="center"  class="body">
-  <tr>
-    <td   height="195" colspan="3"><img src="anhphp/banner.jpg" width="964" height="294"/></td>
-  </tr>
-  <tr>
-    <td height="51" colspan="3"><?php include("header.php");?></td>
-  </tr>
-  <tr>
-    <td width="150"><?php include("theleft.php");?></td>
-    <td width="700"><?php include("themain.php");?></td>
-    <td width="130"><?php include("theright.php");?></td>
-  </tr>
-</table>
+<body>
+<?php 
+    session_start();
+    if(isset($_SESSION['dangnhap']) && $_SESSION['level'] == 1 &&  $_SESSION['status'] == 1){
+		
+?>
+
+<div class="wrapper">
+    <?php
+	        include('modules/config.php');
+			include('modules/header.php');
+			include('modules/menu.php');
+			include('modules/content.php');
+			include('modules/footer.php');
+
+ ?>   
+</div>
+<?php
+	}elseif(isset($_SESSION['dangnhap']) && $_SESSION['level'] == 0 &&  $_SESSION['status'] == 1){
+		?>
+		      <div class="wrapper">
+    <?php
+	        include('modules/config.php');
+			include('modules/header.php');
+			include('modules/menu2.php');
+			include('modules/content2.php');
+			include('modules/footer.php');
+
+ ?>   
+</div>
+<?php
+
+	}elseif(isset($_SESSION['dangnhap']) && $_SESSION['level'] >= 0 &&  $_SESSION['status'] == 0){
+
+    echo "<script> alert('Tài khoản của bạn hiện chưa được kích hoạt !') </script>";
+		header("refresh:0.005;url=login.php");
+	}  else{
+			echo "<script> alert('Vui lòng đăng nhập !') </script>";
+			header("refresh:0.005;url=login.php");
+	}
+	?>
+    
+   
+  
+		
+			
 </body>
+
 </html>
+
+
+
+
+
+
+
+
+
+
